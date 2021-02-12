e375b61f95aeaf601c6712be1faa2a546bdd8093
Tyler Sullens <tyler.sullens@fiscalnote.com>
2021-02-12 13:30:58 -0500

    ignore vpc changes to allow zone sharing

--- modules/zones/main.tf
+++ modules/zones/main.tf
@@ -14,5 +14,12 @@ resource "aws_route53_zone" "this" {
     }
   }
 
+  # https://github.com/hashicorp/terraform/issues/3116
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route53_zone_association
+  # https://github.com/hashicorp/terraform-provider-aws/issues/7812
+  lifecycle {
+    ignore_changes = [vpc]
+  }
+
   tags = lookup(each.value, "tags", null)
 }
